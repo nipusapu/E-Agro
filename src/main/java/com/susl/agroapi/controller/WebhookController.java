6b08ff40c2dfafe958ec19f06165e9e74ff88fab
nipusapu <nipuna.saputhanthri@gmail.com>
2018-07-15 13:11:05 +0530

    update 6

--- src/main/java/com/susl/agroapi/controller/WebhookController.java
+++ src/main/java/com/susl/agroapi/controller/WebhookController.java
@@ -1,18 +1,14 @@
 package com.susl.agroapi.controller;
 
-import ai.api.model.AIOutputContext;
-import ai.api.model.AIResponse;
-import ai.api.model.Fulfillment;
-import ai.api.model.ResponseMessage;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2WebhookRequest;
+import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2WebhookResponse;
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by Kniip on 3/3/2018.
@@ -26,155 +22,20 @@ public class WebhookController {
         String word="";
 
         Gson jsoner = ai.api.GsonFactory.getDefaultFactory().getGson();
-        AIResponse req = jsoner.fromJson(result, AIResponse.class);
+        GoogleCloudDialogflowV2WebhookRequest req = jsoner.fromJson(result,GoogleCloudDialogflowV2WebhookRequest.class);
 
-        String intentName=req.getResult().getMetadata().getIntentName();
+        String intentName=req.getQueryResult().getIntent().getDisplayName();
 
-        if(intentName.equals("Crop_market")){
-            String prameter=req.getResult().getStringParameter("month");
-            prameter=prameter.toLowerCase();
-            if (prameter.matches("january.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("Red Onion has higher market in january");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("february.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("it has no specific crop that has more market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("march.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("it has no specific crop that has more market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("april.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("Green grams has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("may.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("Green grams has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("june.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("Green grams, Potato, tomato has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("july.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("Green grams, Potato, tomato has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("august.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("potato has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("september.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("it has no specific crop that has more market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("october.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("it has no specific crop that has more market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("november.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("potato,Tomato has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("december.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("potato,Tomato has higher market");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-
-        }
-
-        if(intentName.equals("Location_user")) {
-            String prameter = req.getResult().getStringParameter("location");
+        if(intentName.equals("Crop_market")) {
+            String prameter = req.getQueryResult().getParameters().get("month").toString();
             prameter = prameter.toLowerCase();
-            if (prameter.matches("monaragala.*")) {
-                Fulfillment res = new Fulfillment();
+            if (prameter.matches("january.*")) {
+                GoogleCloudDialogflowV2WebhookResponse res = new GoogleCloudDialogflowV2WebhookResponse();
                 res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("You can grow Tomato, Maize, paddy");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("badulla.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("You can grow Potato, tomato, green grams, paddy");
-                res.setMessages(mes);
-                word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-            }
-            if (prameter.matches("walimada.*")) {
-                Fulfillment res = new Fulfillment();
-                res.setSource("webhook");
-                ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                mes.setSpeech("Red onion, beet-root, carrot, tomato are suitable to grow");
-                res.setMessages(mes);
+                res.setFulfillmentText("correct");
                 word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
             }
         }
-        if(intentName.equals("Crop_growing_soil-yes")) {
-           List<AIOutputContext> context = req.getResult().getContexts();
-            for (AIOutputContext item : context){
-                if(item.getName()=="Crop_growing_soil-followup"){
-                    Fulfillment res = new Fulfillment();
-                    res.setSource("webhook");
-                    ResponseMessage.ResponseSpeech mes = new ResponseMessage.ResponseSpeech();
-                    mes.setSpeech("Red onion, beet-root, carrot, tomato are suitable to grow");
-                    res.setMessages(mes);
-                    word = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-
-                }
-            }
-        }
-
 
         return word ;
     }
